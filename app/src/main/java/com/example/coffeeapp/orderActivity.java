5ac05351303e96413cace264c00aa3563c11017c
RoniArbel <57147588+RoniArbel@users.noreply.github.com>
2019-11-18 16:18:22 +0200

    First Version rolling Menu

---
+++ app/src/main/java/com/example/coffeeapp/orderActivity.java
@@ -0,0 +1,14 @@
+package com.example.coffeeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class orderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
