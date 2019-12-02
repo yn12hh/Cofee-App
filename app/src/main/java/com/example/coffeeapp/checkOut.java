9afa0fad7a03babafa6b404d355263f3dfc2119c
AMIT Z <AMITZOO123@GMAIL.COM>
2019-12-02 10:56:03 +0200

    Finished Screen added an intent to "ok go!" button to continue to a screen showing the user that the order was finidshed

--- app/src/main/java/com/example/coffeeapp/checkOut.java
+++ app/src/main/java/com/example/coffeeapp/checkOut.java
@@ -4,21 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+
 public class checkOut extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
         Intent intent = getIntent();
-        String full_order = intent.getStringExtra("size") + " " +
-                intent.getStringExtra("type") + " with " +
-                intent.getStringExtra("milk") + ", " +
-                intent.getStringExtra("sugar") + " tbs of suger. At the temp of **";
-        TextView textView = findViewById(R.id.text_order);
-        textView.setText(full_order);
+
+        TextView type = findViewById(R.id.user_type);
+        TextView size = findViewById(R.id.user_size);
+        TextView milk = findViewById(R.id.user_milk);
+        TextView sugar = findViewById(R.id.user_sugar);
+        TextView temp = findViewById(R.id.user_temp);
+        type.setText(intent.getStringExtra("type"));
+        size.setText(intent.getStringExtra("size"));
+        milk.setText(intent.getStringExtra("milk"));
+        sugar.setText(intent.getStringExtra("sugar"));
+        temp.setText(intent.getStringExtra("temp"));
+    }
+    public void finishOrder(View view) {
+        Intent intent = new Intent(this, endScreen.class);
+        startActivity(intent);
     }
 
 }
